f7f4170de714e123250e9d1a05c695bdc979525a
vivi84 <vivi84@abv.bg>
2025-08-15 19:09:19 +0100

    Add Foody files

---
+++ Foody_Ivaylo/UnitTest1.cs
@@ -0,0 +1,167 @@
+﻿
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Authenticators;
+using System.Net;
+using System.Text.Json;
+
+namespace Foody_Ivaylo
+{
+    [TestFixture]
+    public class FoodyTests
+    {
+        private RestClient _client;
+        private static string createdFoodId;
+        private const string baseUrl = "http://softuni-qa-loadbalancer-2137572849.eu-north-1.elb.amazonaws.com:86";
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            string token = GetJwtToken("vivi84", "123456");
+
+            var options = new RestClientOptions(baseUrl)
+            {
+                Authenticator = new JwtAuthenticator(token)
+            };
+
+            _client = new RestClient(options);
+        }
+        private string GetJwtToken(string username, string password)
+        {
+            var loginClient = new RestClient(baseUrl);
+            var request = new RestRequest("/api/User/Authentication", Method.Post);
+
+            request.AddJsonBody(new { userName = username, password });
+
+            var response = loginClient.Execute(request);
+
+            var json = JsonSerializer.Deserialize<JsonElement>(response.Content);
+
+            return json.GetProperty("accessToken").GetString();
+            
+        }
+
+
+        [Test, Order(1)]
+        public void CreatedFood_ShouldReturnCreated()
+        {
+            var food = new
+            {
+                Name = "New Food",
+                Description = "Delicious new food",
+                Url = ""
+            };
+
+            var request = new RestRequest("/api/Food/Create", Method.Post);
+            request.AddJsonBody(food);
+
+            var response = _client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+
+            var json = JsonSerializer.Deserialize<JsonElement>(response.Content);
+            createdFoodId = json.GetProperty("foodId").GetString() ?? string.Empty;
+            
+            Assert.That(createdFoodId, Is.Not.Null.And.Not.Empty, "Food ID shoulld not be null or Empty.");
+        }
+
+        [Test, Order(2)]
+        public void EditFood_ShouldReturnOk_AndCorrectMessage()
+        {
+            
+            var changes = new[]
+            {
+                new { path = "/name", op = "replace", value = "Updated Food Name" }
+            };
+
+            var request = new RestRequest($"/api/Food/Edit/{createdFoodId}", Method.Patch);
+            request.AddJsonBody(changes);
+
+            var response = _client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            var json = JsonSerializer.Deserialize<JsonElement>(response.Content);
+            var msg = json.GetProperty("msg").GetString();
+
+            Assert.That(msg, Is.EqualTo("Successfully edited"));
+        }
+
+        [Test, Order(3)]
+        public void GetAllFoods_ShouldReturnList()
+        {
+            var request = new RestRequest("/api/Food/All", Method.Get);
+
+            var response = _client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            var foods = JsonSerializer.Deserialize<List<object>>(response.Content);
+            Assert.That(foods, Is.Not.Empty);
+            
+        }
+
+        [Test, Order(4)]
+        public void DeleteFood_ShouldReturnOk()
+        {
+            var request = new RestRequest($"/api/Food/Delete/{createdFoodId}", Method.Delete);
+
+            var response = _client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(response.Content, Does.Contain("Deleted successfully!"));
+        }
+
+        [Test, Order(5)]
+        public void CreateFood_WithoutRequiredFields_ShouldReturnBadRequest()
+        {
+            var food = new 
+            {
+                Name = "",
+                Description = ""
+            };
+            var request = new RestRequest("/api/Food/Create", Method.Post);
+            request.AddJsonBody(food);
+            var response = _client.Execute(request);
+                                    
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        }
+
+        [Test, Order(6)]
+        public void EditNonExistingFood_ShouldReturnNotFound()
+        {
+            string fakeID = "123";
+            var changes = new[]
+            {
+               new { path = "/name", op = "replace", value = "New Title" }
+            };
+            var request = new RestRequest($"/api/Food/Edit/{fakeID}", Method.Patch);
+            request.AddJsonBody(changes);
+
+            var response = _client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+            Assert.That(response.Content, Does.Contain("No food revues..."));
+        }
+
+
+        [Test, Order(7)]
+        public void DeleteNonExistingFood_ShouldReturnBadRequest()
+        {
+            string fakeID = "123";
+            var request = new RestRequest($"/api/Food/Delete/{fakeID}", Method.Delete);
+
+            var response = _client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+            Assert.That(response.Content, Does.Contain("Unable to delete this food revue!"));
+        }
+
+
+        [OneTimeTearDown]
+        public void Cleanup() 
+        {
+            _client?.Dispose();
+        }
+    }
+}
